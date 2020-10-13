27a08f9f961ee3f6a6713e0d200ad6975b191371
roxanaguerra <sguerraapaza@gmail.com>
2020-10-13 12:21:46 -0500

    Pintando datos de prueba del firestore

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { report } from 'process';
+import { SavingService } from '../services/saving.service';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+   savings;
+  constructor(public firestoreService: SavingService) { }
 
   ngOnInit(): void {
+
+    //cargando todos los ahorros de firestore
+    this.firestoreService.getSaving().subscribe(resp => {
+      this.savings = [];
+      resp.forEach((e: any) => {
+        this.savings.push({
+          id: e.payload.doc.data().id,
+          nombre: e.payload.doc.data().nombre,
+          dni: e.payload.doc.data().dni,
+        })        
+      })
+    })
   }
 
 }
