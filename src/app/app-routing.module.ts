77f4732434b970b0c7d1956d224cc2d955caca77
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-10-13 22:41:32 -0500

    change component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from '../app/components/register/register.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+  /*{ path:"inicio",component: AppComponent},*/
   { path: "register",component: RegisterComponent},
-  { path: 'home', component: HomeComponent },
-  { path: 'landing', component: LandingComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
+  { path: "home", component: HomeComponent },
+  { path: "landing", component: LandingComponent },
+  /*{ path: "", redirectTo: "home", pathMatch: 'full' },*/
+  /*{ path: "**", component: PageNotFoundComponent },*/
 
 ];
 
