17ee2ad732b68b2c9ede94060048775bea1f5e14
roxanaguerra <sguerraapaza@gmail.com>
2020-10-15 20:21:32 -0500

    update estilos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,10 +13,11 @@ export class HomeComponent implements OnInit {
   }
 
   nextView(){
-    document.querySelector('.formWelcome').classList.add('hide')
-    document.querySelector('.formDetalleBox').classList.remove('hide')  
+    document.querySelector('.formWelcome').classList.add('hide');
+    document.querySelector('.formDetalleBox').classList.remove('hide');
   }
 
+  // AÑADIR CAJITA NUEVA
   addNewBox(){
     document.querySelector('.formWelcome').classList.add('hide')
     document.querySelector('.formAddBox').classList.remove('hide')
@@ -37,6 +38,16 @@ export class HomeComponent implements OnInit {
   listo(){
     document.querySelector('.formConfimationBox').classList.add('hide')
     document.querySelector('.formWelcome').classList.remove('hide')
+    // document.querySelector('.formAddBox').classList.remove('hide')
   }
 
+  // const functionDisplay = (id) => {
+  //   if (document.getElementById){ //se obtiene el id
+  //   var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+  //   el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+  //   }
+  // };
+  // window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+  //   muestra_oculta('contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+  //   }
 }
